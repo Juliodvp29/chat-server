29ee7b7f01f52e3d6e369ac0e1f31d35fbdd1377
Juliodvp29 <juliodevep29@gmail.com>
2023-08-16 14:58:29 -0500

    db update

--- src/db/chat.sql
+++ src/db/chat.sql
@@ -6,6 +6,7 @@ USE chat_db;
 CREATE TABLE users (
   id INT AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(50) NOT NULL,
+  user_handle VARCHAR(50) NOT NULL UNIQUE,
   email VARCHAR(100) NOT NULL,
   password VARCHAR(100) NOT NULL,
   friends_count int not null default 0,
@@ -71,4 +72,35 @@ BEGIN
 END;
 //
 DELIMITER ;
+
+
+DELIMITER //
+CREATE PROCEDURE ListUserFriends(IN userId INT)
+BEGIN
+  SELECT users.id, users.username, users.user_handle, users.email
+  FROM friendships
+  INNER JOIN users ON (friendships.friend_id = users.id)
+  WHERE friendships.user_id = userId;
+END;
+//
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE ListCommonFriends(IN user1Id INT, IN user2Id INT)
+BEGIN
+  SELECT users.id, users.username, users.user_handle, users.email
+  FROM friendships
+  INNER JOIN users ON (friendships.friend_id = users.id)
+  WHERE friendships.user_id = user1Id IN (
+    SELECT friend_id
+    FROM friendships
+    WHERE user_id = user2Id
+  )
+  LIMIT 10;
+END;
+//
+DELIMITER ;
+
+
+
 
